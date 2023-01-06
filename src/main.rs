cb1dfeee792e031a1a54740bdb8e181b8957ca6a
ramenu <ramenu101@gmail.com>
2023-01-06 14:46:46 -0500

    update function calls

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 
 mod compiler;
 mod walker;
-mod build;
 mod buildtable;
 
 fn main() 
 {
     let mut source_files = Vec::new();
+    let compiler_name = "g++";
     let dir = "./testdir"; // directory to search
-    let out = "./buildinfo";
-    let mut build_table = buildtable::BuildTable::new(out);
+    let mut build_table = buildtable::BuildTable::new();
 
-    walker::retrieve_source_files(dir, &mut source_files, out, &mut build_table);
+    walker::retrieve_source_files(dir, &mut source_files, compiler_name, &mut build_table);
 
-    let compiler_info = compiler::Compiler::new("g++", 
+    let compiler_info = compiler::Compiler::new(compiler_name, 
                                                           &source_files, 
                                                           "-std=c++17",
-                                                          out);
+                                                          "main");
     compiler::compile_to_object_files(&compiler_info);
     build_table.write();
 }
