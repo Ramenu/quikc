bd70b9665b8449afb6f55a6608e0ecb29595a3d5
ramenu <ramenu101@gmail.com>
2023-01-08 13:39:31 -0500

    add more additional checking in the new method

--- src/build.rs
+++ src/build.rs
@@ -1,44 +1,53 @@
-use std::{fs::{self}, process::Command, path::Path};
+use std::{fs::{self}, process::Command, path::Path, io};
 use color_print::{cprintln, cformat};
 use serde_derive::Deserialize;
 
-use crate::{defaultbuild::{GCC_COMPILER_NONEXCLUSIVE_WARNINGS, GCC_COMPILER_C_EXCLUSIVE_WARNINGS, GCC_COMPILER_CPP_DIALECT_OPTIONS, GCC_COMPILER_CPP_EXCLUSIVE_WARNINGS, GCC_STATIC_ANALYSIS_OPTIONS, GCC_AND_CLANG_DIALECT_OPTIONS, CLANG_COMPILER_NONEXCLUSIVE_WARNINGS, CLANG_COMPILER_CPP_WARNINGS, GCC_AND_CLANG_OPTIMIZATION_OPTIONS, GCC_AND_CLANG_ENHANCED_OPTIMIZATION_OPTIONS, GCC_AND_CLANG_LINKER_OPTIONS, GCC_AND_CLANG_CPP_DIALECT_OPTIONS}, compiler::{self, use_default_compiler_configuration}, buildtable::BUILD_TABLE_OBJECT_FILE_DIRECTORY, linker};
+use crate::{defaultbuild::{GCC_COMPILER_NONEXCLUSIVE_WARNINGS, GCC_COMPILER_C_EXCLUSIVE_WARNINGS, GCC_COMPILER_CPP_DIALECT_OPTIONS, GCC_COMPILER_CPP_EXCLUSIVE_WARNINGS, GCC_STATIC_ANALYSIS_OPTIONS, GCC_AND_CLANG_DIALECT_OPTIONS, CLANG_COMPILER_NONEXCLUSIVE_WARNINGS, CLANG_COMPILER_CPP_WARNINGS, GCC_AND_CLANG_OPTIMIZATION_OPTIONS, GCC_AND_CLANG_ENHANCED_OPTIMIZATION_OPTIONS, GCC_AND_CLANG_LINKER_OPTIONS, GCC_AND_CLANG_CPP_DIALECT_OPTIONS}, compiler::{self, use_default_compiler_configuration, select_default_compiler}, buildtable::{BUILD_TABLE_OBJECT_FILE_DIRECTORY, BUILD_TABLE_DIRECTORY}, linker};
 
 const BUILD_CONFIG_FILE : &str = "./Build.toml";
 const BUILD_CONFIG_CACHE_FILE : &str = "./buildinfo/.buildcache";
 
 
-#[derive(Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq, Default)]
 struct Package
 {
     name : String,
     debug_build : bool
 }
 
-#[derive(Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq, Default)]
 struct Compiler
 {
-    compiler : Option<String>,
+    compiler : String,
     args : Option<Vec<String>>,
     cstd : Option<String>,
     cppstd : Option<String>
 }
 
-#[derive(Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq, Default)]
 struct Linker
 {
     args : Option<Vec<String>>,
     libraries : Option<Vec<String>>
 }
 
-#[derive(Deserialize, PartialEq)]
+#[derive(Deserialize, PartialEq, Default, Clone, Copy)]
 struct Misc
 {
     optimization_level : Option<u32>,
     static_analysis_enabled : Option<bool>
 }
 
-#[derive(Deserialize,PartialEq)]
+#[derive(Deserialize, PartialEq, Default)]
+pub struct BuildOption
+{
+    package : Package,
+    compiler : Option<Compiler>,
+    linker : Option<Linker>,
+    misc : Option<Misc>
+}
+
+#[derive(Deserialize, PartialEq, Default)]
 pub struct Build
 {
     package : Package,
@@ -53,51 +62,89 @@ impl Build
     #[inline]
     pub fn new() -> Build
     {
+        if !Path::new(BUILD_TABLE_DIRECTORY).exists() {
+            fs::create_dir(BUILD_TABLE_DIRECTORY).expect("Failed to create directory")
+        }
         let file_contents = fs::read_to_string(BUILD_CONFIG_FILE).expect("Failed to read from build configuration file");
-        let mut toml_config = toml::from_str(&file_contents).expect("Failed to parse build configuration file");
-        let cached_file_contents = fs::read_to_string(BUILD_CONFIG_CACHE_FILE)
-                                              .expect("Failed to read from cached build configuration file");
+        let toml_config : BuildOption = toml::from_str(&file_contents).expect("Failed to parse build configuration file");
+        let mut cached_toml : Option<BuildOption> = None;
+
+        if Path::new(BUILD_CONFIG_CACHE_FILE).exists() {
+            let cached_file_contents = fs::read_to_string(BUILD_CONFIG_CACHE_FILE)
+                                                .expect("Failed to read from cached build configuration file");
+            cached_toml = Some(toml::from_str(&cached_file_contents).expect("Failed to parse cached build configuration file"));
+        }
+        let mut config = Build::default();
+
+        // If a default compiler is not provided, select one automatically
+        if toml_config.compiler.is_some() {
+            if toml_config.compiler.as_ref().unwrap().compiler.is_empty() {
+                config.compiler.compiler = select_default_compiler().to_string();
+                cprintln!("<bold><yellow>note</yellow>:</bold> compiler not specified in 'Build.toml', using {} as default",
+                          config.compiler.compiler);
+            }
+            else {
+                config.compiler.compiler = toml_config.compiler.as_ref().unwrap().compiler.to_owned();
+            }
+            config.compiler.args = toml_config.compiler.as_ref().unwrap().args.to_owned();
+            config.compiler.cppstd = toml_config.compiler.as_ref().unwrap().cppstd.to_owned();
+            config.compiler.cstd = toml_config.compiler.as_ref().unwrap().cstd.to_owned();
+        }
+        else {
+            config.compiler.compiler = select_default_compiler().to_string();
+            cprintln!("<bold><yellow>note</yellow>:</bold> compiler not specified in 'Build.toml', using {} as default",
+                      config.compiler.compiler);
+        }
+
+        // Make sure the cached toml exists before comparing the files, if it doesnt then copy the current toml config
+        // into the cached file
+        if cached_toml.is_some() {
+            // Check if the configuration file has changed since the last build, if so we need to remove all the object
+            // files to recompile them again
+            if toml_config != cached_toml.unwrap() {
+                if Path::new(BUILD_TABLE_OBJECT_FILE_DIRECTORY).is_dir() {
+                    fs::remove_dir_all(BUILD_TABLE_OBJECT_FILE_DIRECTORY).expect("Failed to remove build table object file directory");
+                }
+            }
+        }
+        else {
+            fs::copy(BUILD_CONFIG_FILE, BUILD_CONFIG_CACHE_FILE).expect("Failed to copy contents from build config file to cached config");
+        }
+
+        config.misc = match &toml_config.misc {
+            Some(misc) => *misc,
+            None => Misc {
+                optimization_level : None,
+                static_analysis_enabled : None
+            }
+        };
+
+        config.linker = match toml_config.linker {
+            Some(linker) => linker,
+            None => Linker {
+                args : None,
+                libraries : None
+            }
+        };
+
+        config.package.name = toml_config.package.name;
+        config.package.debug_build = toml_config.package.debug_build;
 
         if !Path::new(BUILD_CONFIG_CACHE_FILE).is_file() {
             fs::copy(BUILD_CONFIG_FILE, BUILD_CONFIG_CACHE_FILE).expect("Failed to cache build configuration file");
-            return toml_config;
+            return config;
         }
 
-
-        let cached_toml = toml::from_str(&cached_file_contents).expect("Failed to parse cached build configuration file");
-
-        // Check if the configuration file has changed since the last build, if so we need to remove all the object
-        // files to recompile them again
-        if toml_config != cached_toml {
-            if Path::new(BUILD_TABLE_OBJECT_FILE_DIRECTORY).is_dir() {
-                fs::remove_dir_all(BUILD_TABLE_OBJECT_FILE_DIRECTORY).expect("Failed to remove build table object file directory");
-            }
-        }
         fs::copy(BUILD_CONFIG_FILE, BUILD_CONFIG_CACHE_FILE).expect("Failed to cache build configuration file");
 
-        // If a default compiler is not provided, select one automatically
-        if toml_config.compiler.compiler.is_none() {
-            toml_config.compiler.compiler = compiler::select_default_compiler();
-            if toml_config.compiler.compiler.is_none() {
-                eprintln!("{}", cformat!("<red><bold>error:</bold></red> Could not find a default compiler to use.
-                                          Please specify your own in the 'build.toml' file"));
-                std::process::exit(1);
-            }
-            else {
-                cprintln!("<bold><yellow>note:</yellow></bold> Compiler not set in 'Build.toml', using {} as default", 
-                          toml_config.compiler.compiler.as_ref().unwrap());
-            }
-        }
-
-        return toml_config;
+        return config;
     }
 
     pub fn execute_compiler_with_build_info(&self, file : &String) -> Command
     {
-        let compiler_name = self.compiler.compiler.as_ref().unwrap();
         let compiler_args = &self.compiler.args;
 
-        let mut cmd = Command::new(compiler_name);
+        let mut cmd = Command::new(&self.compiler.compiler);
         // The only variable that cannot really be overridden is whether or not
         // the build is being compiled in debug mode
         if self.package.debug_build {
@@ -132,7 +179,7 @@ impl Build
             }
 
             // Default configuration only supported on gcc and clang
-            if compiler::is_gcc_or_clang(compiler_name) {
+            if compiler::is_gcc_or_clang(&self.compiler.compiler) {
 
                 cmd.args(GCC_AND_CLANG_DIALECT_OPTIONS);
 
@@ -156,7 +203,7 @@ impl Build
                 }
 
                 
-                match compiler_name.as_str() {
+                match self.compiler.compiler.as_str() {
                     "gcc"|"g++" => {
                         cmd.args(GCC_COMPILER_NONEXCLUSIVE_WARNINGS);
                         if is_c_source_file {
@@ -182,7 +229,7 @@ impl Build
                 }   
             }
             else {
-                cprintln!("<bold><yellow>note:</yellow></bold> Cannot use default configuration because
+                cprintln!("<bold><yellow>note:</yellow></bold> cannot use default configuration because
                            compiler vendor is unknown, please supply your own flags.");
             }
             return cmd;
@@ -198,10 +245,9 @@ impl Build
 
     pub fn execute_linker_with_build_info(&self) -> Command
     {
-        let compiler_name = self.compiler.compiler.as_ref().unwrap();
         let linker_libraries = self.linker.libraries.as_ref();
 
-        let mut cmd = Command::new(compiler_name);
+        let mut cmd = Command::new(&self.compiler.compiler);
 
         if linker::use_default_linker_configuration(&self.linker.args) {
             // apply linker optimizations on release builds only
@@ -228,7 +274,12 @@ impl Build
     }
 
     #[inline]
+    pub fn is_debug_build(&self) -> bool {
+        return self.package.debug_build;
+    }
+
+    #[inline]
     pub fn get_compiler_name(&self) -> &String {
-        return &self.compiler.compiler.as_ref().unwrap();
+        return &self.compiler.compiler;
     }
 }
