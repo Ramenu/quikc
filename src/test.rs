0d5fc724d672b4465761263b984e0dd9364bb26b
ramenu <ramenu101@gmail.com>
2023-01-15 16:32:53 -0500

    add test_first_time_compilation

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,45 @@
 use std::{process::Command, fs, env, path::Path, io};
 
-use crate::{build::BUILD_CONFIG_FILE, SOURCE_DIRECTORY, compiler::INCLUDE_PATH};
+use jwalk::WalkDir;
+
+use crate::{build::{BUILD_CONFIG_FILE, Build}, SOURCE_DIRECTORY, compiler::{INCLUDE_PATH, compile_to_object_files}, buildtable::{BuildTable, BUILD_TABLE_OBJECT_FILE_DIRECTORY}, walker, linker::link_files};
+
+
+struct Tools
+{
+    build_config : Build,
+    source_files : Vec<String>,
+    old_table : toml::value::Table,
+    build_table : BuildTable
+}
+
+impl Tools
+{
+    pub fn new() -> Tools
+    {
+        let build_config = Build::new();
+        let mut old_table = toml::value::Table::new();
+        let source_files = Vec::new();
+        let build_table = BuildTable::new(&mut old_table);
+
+        return Tools {
+            build_config,
+            source_files,
+            old_table,
+            build_table
+        };
+    }
+}
+
+#[inline]
+fn get_src_files(tools : &mut Tools)
+{
+    walker::retrieve_source_files(SOURCE_DIRECTORY, 
+                                  &mut tools.source_files, 
+                                  &tools.build_config.get_compiler_name(), 
+                                  &mut tools.build_table,
+                                  &mut tools.old_table);
+}
 
 /// Initializes the project. If you the 'setup_additional_files' parameter is set to true,
 /// then the function will copy additional source files to 'src'.
@@ -69,8 +108,18 @@ fn test_quikc_init() ->  Result<(), Box<dyn std::error::Error>>
 fn test_first_time_compilation() -> Result<(), Box<dyn std::error::Error>>
 {
     initialize_project(true)?;
+    let mut tools = Tools::new();
+    get_src_files(&mut tools);
+
+    const TOTAL_SOURCE_FILES : usize = 2;
+    assert_eq!(tools.source_files.len(), TOTAL_SOURCE_FILES);
+
+    let compilation_success = compile_to_object_files(&mut tools.source_files, &tools.build_config);
+    assert_eq!(compilation_success, true);
+    assert_eq!(fs::read_dir(BUILD_TABLE_OBJECT_FILE_DIRECTORY)?.count(), TOTAL_SOURCE_FILES);
+
+    let link_success = link_files(&tools.build_config);
+    assert_eq!(link_success, true);
     
-
     Ok(())
-
 }
