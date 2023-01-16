626fbccc6fb19608be5792ea6b1d11adcac34fc4
ramenu <ramenu101@gmail.com>
2023-01-15 21:49:50 -0500

    add testfiles dir

---
+++ testfiles/hi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hi.h"
+
+void hi(const char *name)
+{
+    printf("Hi, %s\n", name);
+}
---
+++ testfiles/invalid/invalid.c
@@ -0,0 +1,11 @@
+// This file has a syntax error on purpose. Should be used
+// for testing when you want to check if the file gets
+// recompiled. 
+
+#include "hi.h"
+
+int doesnt_work()
+{
+    hi("friend!");
+    return 0
+}
