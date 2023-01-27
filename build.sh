daf3abd6675c82e62917d4b0356b90253aafeade
ramenu <ramenu101@gmail.com>
2023-01-27 17:49:41 -0500

    build: run clippy before building

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Don't run this script if you're going to distribute the binary
 # just use 'cargo build --release' 
 
-RUSTFLAGS="-C target-cpu=native" cargo build --release
+cargo clippy && RUSTFLAGS="-C target-cpu=native" cargo build --release
