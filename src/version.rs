ba3329869cfccdb6d42b57f21b1c87ff64f804d9
ramenu <ramenu101@gmail.com>
2023-01-28 07:47:21 -0500

    fix: version number

--- src/version.rs
+++ src/version.rs
@@ -1,18 +1,18 @@
 use const_format::concatcp;
 
 pub const VERSIONS : [&str; 1] = [
-    concatcp!("1", ".", MINOR_VERSIONS[0], ".", PATCH_VERSIONS[0])
+    concatcp!("0", ".", MINOR_VERSIONS[0], ".", PATCH_VERSIONS[0])
+];
+
+pub const MINOR_VERSIONS : [&str; 1] = [
+    "1"
+];
+
+pub const PATCH_VERSIONS : [&str; 1] = [
+    "0"
 ];
 
 #[cfg(feature = "quikc-nightly")]
 pub const NIGHTLY_VERSIONS : [&str; 1] = [
     concatcp!(VERSIONS[0], "-nightly")
-];
-
-pub const MINOR_VERSIONS : [&str; 1] = [
-    "0"
-];
-
-pub const PATCH_VERSIONS : [&str; 1] = [
-    "0"
 ];
