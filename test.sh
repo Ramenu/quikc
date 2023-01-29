a5953615d4a980ac285378c9c0b9eb0d0978b8a3
ramenu <ramenu101@gmail.com>
2023-01-29 18:28:58 -0500

    test: update scripts

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cargo test test_all
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ "$branch_name" == "nightly" || "$branch_name" == "nightly-dev" ]]; then
+    cargo clippy --features "quikc-nightly" && cargo test test_all --features quikc-nightly
+else
+    cargo clippy && cargo test test_all
+fi
