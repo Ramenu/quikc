e2f3ac697578b549a675d66ec193e902ba1cc020
ramenu <ramenu101@gmail.com>
2023-02-11 16:34:22 -0500

    bump version

--- src/version.rs
+++ src/version.rs
@@ -16,6 +16,7 @@ const _END_MV : usize = (line!() as usize) - _BEGIN_MV - 3;
 
 const _BEGIN_PV : usize = line!() as usize;
 pub const PATCH_VERSIONS : [&str; _END_PV] = [
+    "3",
     "2",
     "1",
     "0"
