3f6f10f6d9b6190ccf06601832257e4138ba7e83
ramenu <ramenu101@gmail.com>
2023-02-24 14:31:41 -0500

    build: add '-d' arg

--- build.sh
+++ build.sh
@@ -7,8 +7,16 @@
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
 if [[ "$branch_name" == "nightly" || "$branch_name" == "nightly-dev" ]]; then
+    if [[ "$1" == "-d" ]]; then
+        cargo clippy --features "quikc-nightly" && cargo build --features "quikc-nightly"
+        exit
+    fi
     cargo clippy --features "quikc-nightly" && RUSTFLAGS="-C target-cpu=native" cargo build --release --features "quikc-nightly"
 else
+    if [[ "$1" == "-d" ]]; then
+        cargo clippy && cargo build
+        exit
+    fi
     cargo clippy && RUSTFLAGS="-C target-cpu=native" cargo build --release
 fi
 
